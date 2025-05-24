abc44fde9129bd371e97faec367366e778a912fc
wDeloo <wdelo.hack@gmail.com>
2025-05-24 11:05:08 +0200

    nav

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,15 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
+  output: "static",
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [react()]
 });
